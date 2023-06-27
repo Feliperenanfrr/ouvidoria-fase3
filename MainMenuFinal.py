1a5c4c4810ce5a7965467783c7f298851283ea9e
Matheus Clemente <matheus.pc2000@hotmail.com>
2023-06-26 22:31:31 -0300

    Primeira versão do Menu com as funcionalidades listagem e listagem por tipo

---
+++ MainMenuFinal.py
@@ -0,0 +1,45 @@
+"""
+Programação em linguagem estruturada
+FASE 2
+Alunos:
+    Matheus Pinto Clemente - RA: 2313080132
+    Felipe Renan dos Santos Ferreira - RA: 2313080103
+    Mateus Patricio Vasconcelos - RA: 2313080094
+    Wilami Ferreira de Pontes - RA: 2313080119
+    Luan Rodrigo Ribeiro de Souza - RA: 2313080078
+"""
+
+from operacoesbd import *
+from ouvidoriaFinal import *
+
+conexao = abrirBancoDados('localhost', 'root', 'Mat16062000!', 'ouvidoria1')
+
+print('BEM VINDO À OUVIDORIA')
+opcao = 1
+
+while opcao != 0:
+
+    print('[  1  ] Listagem de manifestações')
+    print('[  2  ] Listagem de manifestações por tipo')
+    print('[  3  ] Criar uma nova manifestação')
+    print('[  4  ] Exibir quantidade de manifestações')
+    print('[  5  ] Pesquisar manifestação por ID')
+    print('[  6  ] Modificar manifestação por ID')
+    print('[  7  ] Excluir manifestação pelo ID')
+    print('[  0  ] Sair do sistema')
+    opcao = int(input('DIGITE SUA OPÇÃO: '))
+
+    if opcao == 1:
+        titulos('LISTANDO MANIFESTAÇÕES')
+        manifestacoes = listarManifestacoes(conexao)
+        for manifestacao in manifestacoes:
+            print(manifestacao)
+
+
+    elif opcao == 2:
+        print('LISTAGEM POR MANIFESTAÇÃO')
+        print('Qual tipo de manifestação que quer visualizar\n1 - Reclamação\n2 - Sugestão\n3 - Elogio')
+        tipo = int(input('Digite sua opção: '))
+        manifestacoes_tipo = listarPorTipo(conexao, tipo)
+        for manifestacao in manifestacoes_tipo:
+            print(manifestacao)
