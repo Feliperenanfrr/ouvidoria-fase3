e93e169d6c41994d9898eddef3f4708cbbcc3f30
Matheus Clemente <matheus.pc2000@hotmail.com>
2023-06-26 22:34:32 -0300

    Primeira versão das funções contém os def's da listagem e listagem por tipo

---
+++ ouvidoriaFinal.py
@@ -0,0 +1,39 @@
+from operacoesbd import *
+
+def titulos(texto):
+    print('-' * 60)
+    print(f'{texto:^60}')
+    print('-' * 60)
+
+def listarManifestacoes(conexao):
+    consultaListagem = 'select * from ocorrencias'
+    ouvidoria = listarBancoDados(conexao, consultaListagem)
+
+    if len(ouvidoria) != 0:
+        manifestacoes = []
+        for item in ouvidoria:
+            tipo = ""
+            if item[1] == 1:
+                tipo = "Reclamação"
+            elif item[1] == 2:
+                tipo = "Sugestão"
+            elif item[1] == 3:
+                tipo = "Elogio"
+            manifestacoes.append(f'ID {item[0]} - Título: {item[2]} - Autor: {item[4]} - {tipo}')
+        return manifestacoes
+    else:
+        return 'Não há manifestações cadastradas'
+
+
+
+def listarPorTipo(conexao,tipo):
+    consultaListagem = f'SELECT * FROM ocorrencias WHERE tipo = {tipo}'
+    ouvidoria = listarBancoDados(conexao, consultaListagem)
+
+    if len(ouvidoria) != 0:
+        manifestacoes = []
+        for item in ouvidoria:
+            manifestacoes.append(f'ID {item[0]} - Título: {item[2]} - Autor: {item[4]}')
+        return manifestacoes
+    else:
+        return 'Não há manifestações desse tipo'
